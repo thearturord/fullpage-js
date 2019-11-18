f895052d66923a87e8d097acd7dbd96ad0b36a4f
thearturord <47672781+thearturord@users.noreply.github.com>
2019-11-18 18:32:34 +0100

    Update taza-1.php

--- taza-php/taza-1.php
+++ taza-php/taza-1.php
@@ -5,28 +5,32 @@ include "../variables.php";
 $titulo = $_POST['titulo'];
 $subtitulo = $_POST['subTitulo'];
 
-$reading = fopen('../variables.php', 'r');
-$writing = fopen('../variables.txt', 'w');
 
-$replaced = false;
+echo $titulo;
+echo $taza0;
 
-while (!feof($reading)) {
-  $line = fgets($reading);
-  if (stristr($line,$taza0)) {
-    $line = " \"$titulo\" \n";
-    $replaced = true;
-  }
-  fputs($writing, $line);
-}
-fclose($reading); fclose($writing);
-// might as well not overwrite the file if we didn't replace anything
-if ($replaced)
-{
-  rename('../variables.txt', '../variables.php');
-} else {
-  unlink('../variables.txt');
-}
-
-header ('Location: ../message-sent.html');
+// $reading = fopen('../variables.php', 'r');
+// $writing = fopen('../variables.txt', 'w');
+//
+// $replaced = false;
+//
+// while (!feof($reading)) {
+//   $line = fgets($reading);
+//   if (stristr($line,$taza0)) {
+//     $line = " \"$titulo\" \n";
+//     $replaced = true;
+//   }
+//   fputs($writing, $line);
+// }
+// fclose($reading); fclose($writing);
+// // might as well not overwrite the file if we didn't replace anything
+// if ($replaced)
+// {
+//   rename('../variables.txt', '../variables.php');
+// } else {
+//   unlink('../variables.txt');
+// }
+//
+// header ('Location: ../message-sent.html');
 
 ?>
